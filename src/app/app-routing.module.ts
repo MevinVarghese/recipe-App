bc2ff3eb51f02d258ac3122c550eb3088c957c75
Mevin Varghese <mevinvarghese2000@gmail.com>
2024-12-20 22:31:18 +0530

    view-page-created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewPageComponent } from './view-page/view-page.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainViewComponent },
+  {
+    path: 'view-page',
+    component: ViewPageComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
